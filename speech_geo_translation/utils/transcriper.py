4bcdb7aa95fdc4545131f746a171ab87aaedc7b5
abuelnasr0 <abuelnasr70@gmail.com>
2024-09-10 20:15:20 +0300

    Add english transcription

--- speech_geo_translation/utils/transcriper.py
+++ speech_geo_translation/utils/transcriper.py
@@ -1,11 +1,29 @@
 import nemo.collections.asr as nemo_asr
 
-class Transcriper:
-    def __init__(self, ):
-        self.model = nemo_asr.models.EncDecCTCModelBPE.restore_from("/home/mohamed/Mohamed/Vodafone project/Conformer-CTC-L_spe128_ar-AR_3.0.nemo")
+from enum import Enum
 
-    def __call__(self, audio_path):
-        return self.model.transcribe([audio_path])[0]
-    
-    def transcribe(self, audio_path):
+
+class Language(Enum):
+    AR = 1
+    EN = 2
+
+
+class Transcriper:
+    def __init__(
+        self,
+    ):
+        self.ar_model = nemo_asr.models.EncDecCTCModelBPE.restore_from(
+            "/home/mohamed/Mohamed/Vodafone project/Conformer-CTC-L_spe128_ar-AR_3.0.nemo"
+        )
+        self.en_model = nemo_asr.models.EncDecCTCModelBPE.restore_from(
+            "/home/mohamed/Mohamed/Vodafone project/stt_en_conformer_ctc_large.nemo"
+        )
+
+    def __call__(self, audio_path: str, language: Language = Language.AR):
+        if language == Language.AR:
+            return self.ar_model.transcribe([audio_path])[0]
+        elif language == Language.EN:
+            return self.en_model.transcribe([audio_path])[0]
+
+    def transcribe(self, audio_path: str, language: Language = Language.AR):
         return self.__call__(audio_path=audio_path)
