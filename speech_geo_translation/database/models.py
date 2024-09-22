25fe238fb1f571f1d37b41513a7046c4a2fb24f2
abuelnasr0 <abuelnasr70@gmail.com>
2024-09-22 17:37:39 +0300

    Add new tables

--- speech_geo_translation/database/models.py
+++ speech_geo_translation/database/models.py
@@ -8,18 +8,62 @@ class Base(DeclarativeBase):
     pass
 
 
-class Complaint(Base):
-    __tablename__ = "complaint"
+class Customer(Base):
+    __tablename__ = "customer"
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=True)
+    phone_number: Mapped[str] = mapped_column(String(11), nullable=True, unique=True)
+
+
+class Governorate(Base):
+    __tablename__ = "governorate"
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+
+
+class Qism(Base):
+    # section, (qism or markaz)
+    __tablename__ = "qism"
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    gov_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("governorate.id"), nullable=False
+    )
+    name: Mapped[str] = mapped_column(String(50))
+
+
+class ServiceArea(Base):
+    # Sub section, (Shayakha or village)
+    __tablename__ = "service_area"
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    governorate_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("governorate.id"), nullable=False
+    )
+    qism_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("qism.id"), nullable=False
+    )
+    name: Mapped[str] = mapped_column(String(50))
+
+
+class ComplaintLocation(Base):
+    __tablename__ = "complaint_location"
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
     longitude: Mapped[float] = mapped_column(Float, nullable=False)
     latitude: Mapped[float] = mapped_column(Float, nullable=False)
-    time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    complaint_type: Mapped[int] = mapped_column(
-        BigInteger, ForeignKey("complaint_type.id")
+    service_area_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("service_area.id"), nullable=False
     )
 
 
-class ComplaintType(Base):
-    __tablename__ = "complaint_type"
+class Complaint(Base):
+    __tablename__ = "complaint"
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
-    type: Mapped[str] = mapped_column(String(20))
+    customer_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("customer.id"), nullable=True
+    )
+    location_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey("complaint_location.id"), nullable=False
+    )
+    time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    type: Mapped[str] = mapped_column(String(50), nullable=True)
+    status: Mapped[str] = mapped_column(String(50), nullable=True)
+    priority: Mapped[int] = mapped_column(int, nullable=True)
